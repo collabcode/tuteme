95e709dd94da5b570116010f996d6cb8c608e775
Ramesh <ramesh.mc@gmail.com>
2024-12-26 15:48:25 +0000

    working

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,41 @@
 const storyContainer = document.getElementById('story-container');
+const progressBar = document.getElementById('progress-bar'); // Get the progress bar element
 let currentStepIndex = 0;
 let storyData = [];
 let storyName = '';
+let totalAssets = 0;
+let loadedAssets = 0;
+
+function preloadAssets() {
+  return new Promise((resolve) => {
+    const promises = [];
+    let i =0;
+    storyData.forEach(step => {
+      if (step.ElementId) {
+        // Preload image
+        const img = new Image();
+        img.src = `data/${storyName}/images/${step.ElementId}.png`;
+        promises.push(new Promise(resolve => img.onload = resolve)); 
+
+        // Preload audio
+        const audio = new Audio(`data/${storyName}/audio/${i}.mp3`); 
+        promises.push(new Promise(resolve => audio.oncanplaythrough = resolve)); 
+      }
+      i++;
+    });
+
+    totalAssets = promises.length; // Total number of assets to load
+
+    Promise.all(promises).then(() => {
+      resolve();
+    });
+  });
+}
+
+function updateProgressBar() {
+  const progress = (loadedAssets / totalAssets) * 100;
+  progressBar.style.width = `${progress}%`;
+}
 
 function playStory() {
   //clear
@@ -41,8 +75,8 @@ function playStory() {
     }
 
     gsap.to(element, {
-      opacity: 1,
-      duration: 0.5,
+      opacity: 10,
+      duration: 2,
     }).eventCallback('onComplete', () => {
       // Play the corresponding audio file
       const audio = new Audio(`data/${storyName}/audio/${currentStepIndex}.mp3`); 
@@ -51,7 +85,6 @@ function playStory() {
       currentStepIndex++;
       const waitTime = (moment.duration(currentStep.EndTime, 'mm:ss:SS').asMilliseconds() - 
                        moment.duration(currentStep.StartTime, 'mm:ss:SS').asMilliseconds())/100;
-          
       console.log(waitTime)
       setTimeout(playStory, waitTime);
     });
@@ -64,7 +97,9 @@ fetch(`data/${storyname}/story.json`)
   .then(data => {
     storyName = storyname;
     storyData = data; 
-    playStory(); 
+    preloadAssets().then(() => {
+      playStory(); 
+    });
   })
   .catch(error => {
     console.error('Error loading story data:', error);
