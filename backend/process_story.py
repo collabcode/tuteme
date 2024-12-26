49a1176e79ed3d7308b5e10f1a61d4fded89c3b6
Ramesh <ramesh.mc@gmail.com>
2024-12-26 13:18:49 +0000

    working

--- backend/process_story.py
+++ backend/process_story.py
@@ -1,4 +1,5 @@
 import os
+import azure.cognitiveservices.speech as speechsdk
 import csv
 import json
 import shutil
@@ -28,6 +29,38 @@ def csv_to_json(csv_file, json_file):
     with open(json_file, 'w') as jsonfile:
         json.dump(data, jsonfile, indent=4)
 
+    for idx, row in enumerate(data):
+        if row['Narration']:
+            print(f"{idx}/{row['Narration']}")
+            audio_filename = f"data/story1/audio/{idx}.mp3" 
+            createAudioFile(row['Narration'], audio_filename)
+            
+
+def createAudioFile(text, filenameWithPath):
+    # return #To not call this every time
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+    # audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    audio_config = speechsdk.audio.AudioOutputConfig(filename=filenameWithPath)
+
+    # The neural multilingual voice can speak different languages based on the input text.
+    speech_config.speech_synthesis_voice_name='en-US-AvaMultilingualNeural'
+
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+    speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+    if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+        print("Speech synthesized for text [{}]".format(text))
+    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_synthesis_result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(cancellation_details.error_details))
+                print("Did you set the speech resource key and region values?")
+
 csv_file = 'data/story1/story.csv'
 json_file = 'public/data/story1/story.json' 
+
 csv_to_json(csv_file, json_file)
